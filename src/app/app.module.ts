95ae70d9d7ab300ec94345db170dc29e55379e35
Agustin Salto <agustinnicolassalto@gmail.com>
2023-10-13 16:27:39 -0300

    fix typo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/couter.compoent';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
