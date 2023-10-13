b08ca141d6671992cb88160fab3d35e049b93109
Agustin Salto <agustinnicolassalto@gmail.com>
2023-10-13 18:02:47 -0300

    Add capitalize name method

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 export class HeroComponent {
   public name: string = 'ironman';
   public age: number = 45;
+
+
+  get capitalizedName() : string {
+    return this.name.toUpperCase();
+  }
+
+
 }
